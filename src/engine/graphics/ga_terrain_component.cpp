d35428ec96e272160ad3ad56fa7f4dcc4bb12830
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2017-04-13 15:46:36 -0400

    Added boilerplate for terrain component

---
+++ src/engine/graphics/ga_terrain_component.cpp
@@ -0,0 +1,18 @@
+/*
+** RPI Game Architecture 2017
+** Final project - Ian Chamberlain
+** 
+** Terrain generator component
+*/
+
+#include "ga_terrain_component.h"
+
+
+ga_terrain_component::ga_terrain_component()
+{
+}
+
+
+ga_terrain_component::~ga_terrain_component()
+{
+}
---
+++ src/engine/graphics/ga_terrain_component.h
@@ -0,0 +1,9 @@
+
+#include "entity/ga_component.h"
+
+class ga_terrain_component : public ga_component
+{
+public:
+	ga_terrain_component();
+	virtual ~ga_terrain_component();
+};
