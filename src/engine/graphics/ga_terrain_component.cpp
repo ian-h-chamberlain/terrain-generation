4afc712a1ad9363aa2b7bdfc48de66a35729e714
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2017-04-14 15:49:02 -0400

    Some more boilerplate, getting ready to draw a simple quad-based mesh

--- src/engine/graphics/ga_terrain_component.cpp
+++ src/engine/graphics/ga_terrain_component.cpp
@@ -6,13 +6,43 @@
 */
 
 #include "ga_terrain_component.h"
+#include "ga_material.h"
 
 
-ga_terrain_component::ga_terrain_component()
+ga_terrain_component::ga_terrain_component(ga_entity* ent, const char* param_file) : ga_component(ent)
 {
-}
+	_material = new ga_constant_color_material();
+	_material->init();
+	_material->set_color({0.6f, 0.6f, 0.6f});
 
+	// TODO actually generate the terrain
+	static GLfloat vertices[] = {
+		-1.0, -1.0, -1.0,
+		 1.0, -1.0, -1.0,
+		 1.0, -1.0,  1.0,
+		-1.0, -1.0,  1.0,
+	};
+
+	static GLushort indices[] = {
+		0,  1,  2,
+		2,  3,  0,
+	}
+
+	// TODO setup VBOs for drawing
+
+	// possible implementation for infinite: keep a list of neighbors and generate them as needed
+}
 
 ga_terrain_component::~ga_terrain_component()
 {
+	// TODO delete VBOs
+
+	delete _material;
+}
+
+void ga_terrain_component::update(ga_frame_params * params)
+{
+	// TODO draw terrain
+
+	// TODO generate further terrain based on camera position?
 }
--- src/engine/graphics/ga_terrain_component.h
+++ src/engine/graphics/ga_terrain_component.h
@@ -4,6 +4,14 @@
 class ga_terrain_component : public ga_component
 {
 public:
-	ga_terrain_component();
+	ga_terrain_component(class ga_entity* ent, const char* param_file);
 	virtual ~ga_terrain_component();
+
+	virtual void update(struct ga_frame_params* params) override;
+
+private:
+	class ga_material* _material;
+	uint32_t _vao;
+	uint32_t _vbos[4];
+	uint32_t _index_count;
 };
