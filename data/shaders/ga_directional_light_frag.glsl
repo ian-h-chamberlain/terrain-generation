28df8e54acf4772dc2eebc6c536c0dc0879be1e3
Ian Chamberlain <ian.h.chamberlain@gmail.com>
2017-04-19 22:51:39 -0400

    Better shader for visualizing quad and triangles

--- data/shaders/ga_directional_light_frag.glsl
+++ data/shaders/ga_directional_light_frag.glsl
@@ -1,12 +1,23 @@
 #version 400
 
 uniform vec3 u_color;
-uniform vec3 u_light_direction;
 
-in float height;
+in vec3 world_position;
 
 void main(void)
 {
-	vec3 c = u_color * height;
-	gl_FragColor = vec4(c, 1.0);
+	vec3 c = u_color;
+	float grid_thickness = 0.03;
+
+	bool on_grid = fract (world_position.x * 2.0) < grid_thickness ||
+		fract (world_position.z * 2.0) < grid_thickness ||
+		fract ((world_position.x - world_position.z) * 2.0) < grid_thickness;
+
+	if (on_grid)
+	{
+		gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);
+	}
+	else {
+		gl_FragColor = vec4(c, 1.0);
+	}
 }
--- data/shaders/ga_directional_light_vert.glsl
+++ data/shaders/ga_directional_light_vert.glsl
@@ -6,9 +6,10 @@ uniform mat4 u_proj;
 layout(location = 0) in vec3 in_vertex;
 
 out float height;
+out vec3 world_position;
 
 void main(void)
 {
 	gl_Position = vec4(in_vertex, 1.0) * u_mvp;
-	height = clamp(in_vertex.y, 0.3, 1.0);
+	world_position = in_vertex;
 }
